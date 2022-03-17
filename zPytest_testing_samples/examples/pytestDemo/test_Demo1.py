9eff270c2736e4072532ccba5d4eed753547a62a
Triumph <triumphrazor@gmail.com>
2022-03-17 22:22:37 +0530

    added pytest demo

---
+++ zPytest_testing_samples/examples/pytestDemo/test_Demo1.py
@@ -0,0 +1,8 @@
+# Any pytest file should start with test_
+# pytest method names should start with test
+# Any code should be wrapped in method
+
+
+def test_firstProgram():
+    """To test the first Program of pytest"""
+    print("Hello Sourabh")
